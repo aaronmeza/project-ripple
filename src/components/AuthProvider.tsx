9d6dc27bb33f6322e356d9bab4f8feb63bbf60d8
Aaron Meza <aaron.meza@gmail.com>
2025-06-04 03:00:00 -0500

    build fix session data again

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -8,7 +8,7 @@ import { Session } from '@supabase/supabase-js'
 export const AuthContext = createContext<{ session: Session | null }>({ session: null })
 
 export function AuthProvider({ children }: PropsWithChildren) {
-  const [session, setSession] = useState<Session | null>(supabase.auth.getSession() || null)
+  const [session, setSession] = useState<Session | null>(supabase.auth.getSession())
 
   useEffect(() => {
     const { data: listener } = supabase.auth.onAuthStateChange((_event, sess) => {
