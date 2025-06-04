5229e22f793de54854abfa61b021c64ea763233f
Aaron Meza <aaron.meza@gmail.com>
2025-06-04 03:08:42 -0500

    correct subscription unsubscripe on AuthProvider

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -10,10 +10,10 @@ export const AuthContext = createContext<{ session: Session | null }>({ session:
 export function AuthProvider({ children }: PropsWithChildren) {
 const [session, setSession] = useState<Session | null>(null)
   useEffect(() => {
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, sess) => {
+    const { data: subscription } = supabase.auth.onAuthStateChange((_event, sess) => {
       setSession(sess)
     })
-    return () => listener?.unsubscribe()
+    return () => subscription.unsubscribe()
   }, [])
 
   return <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
